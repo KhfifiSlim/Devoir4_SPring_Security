452f34696d161f6138aee5681f42ec62bd279126
khfifi_slim <80694671+KhfifiSlim@users.noreply.github.com>
2021-10-26 20:19:18 +0100

    Add files via upload

---
+++ users-microservice/src/main/java/com/slim/users/service/UserService.java
@@ -0,0 +1,7 @@
+package com.slim.users.service;
+
+import com.slim.users.entities.Role;
+import com.slim.users.entities.User;
+public interface UserService { User saveUser(User user); User findUserByUsername (String username);
+Role addRole(Role role); User addRoleToUser(String username, String rolename);
+}
